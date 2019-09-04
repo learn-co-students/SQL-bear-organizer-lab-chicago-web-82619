bc80cf1ce6d3a7cbd8423461833384ab58418abb
justrmarks <blackbirdwanderer@gmail.com>
2019-09-04 15:41:26 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+  ('Mr. Chocolate', 20, "Female", "Brown", "Moody", "1"),
+  ('Rowdy', 4, "Female", "Brown", "Moody", "1"),
+  ('Tabitha', 7, "Female", "Brown", "Moody", "1"),
+  ('Sergeant Brown', 23, "Female", "Brown", "Moody", "1"),
+  ('Melissa', 5, "Female", "Brown", "Moody", "1"),
+  ('Grinch', 9, "Female", "Brown", "Moody", "1"),
+  ('Wendy', 3, "Female", "Brown", "Moody", "1"),
+  (NULL, 3, "Female", "Brown", "Moody", "1");
